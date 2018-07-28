46cc1a49983adf94e6a1a7416f7b8130e628584e
RPeterC98 <34779281+RPeterC98@users.noreply.github.com>
2018-07-28 16:00:20 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+
+app.listen(port, () => console.log('Listening on port:' + port));
